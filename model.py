43d3c76cfdd4e77e6a25b9e852021b9ef5cb44fa
Arjun <arj.kashyap123@gmail.com>
2020-07-23 07:04:16 +0530

    Increased accuracy to 70% via transfer learning vgg16

--- model.py
+++ model.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3.6
 
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras import regularizers, applications
 import numpy as np
 from sklearn.model_selection import train_test_split
-from keras import regularizers
+from tensorflow.keras import regularizers
 import time
 
 NAME = "dance-form-classifier-{}".format(time.strftime("%d%h-%m-%S"))
 
 #Model name for tensor board
-tensorboard = TensorBoard(log_dir="""logs/{}""".format(NAME))
+tensorboard = TensorBoard(log_dir="""logs\{}""".format(NAME))
+CLASSES=8
 classes = [ x for x in range(8)]
 
 
@@ -33,43 +35,42 @@ bs = 32         # Batch size
 # CNN Model
 def cnn_model(X_train, X_test , y_train, y_test):
 
+    base_model = applications.VGG16(include_top=False, input_shape=X_train.shape[1:], weights='imagenet',classes=CLASSES)
+
+    # Freezing VGG16 layers
+    for layer in base_model.layers:
+        layer.trainable=False
+    
+    last_layer = 'block5_pool'
+    model = Model(base_model.input, base_model.get_layer(last_layer).output)
+
+    model.layers[-1].output.shape
     model = Sequential()
-    # Layer I
-    model.add(Conv2D( 64, (3, 3), activation="relu", input_shape=X.shape[1:] ))
+
+    model.add(base_model)      # Stack vgg16 
+
+    model.add(Conv2D(128,(3,3),activation="relu", input_shape=model.layers[-1].output.shape, data_format='channels_first'))
     model.add(MaxPooling2D(2,2))
 
-    # Layer II
-    model.add(Conv2D(128,(3,3),activation="relu"))
-    model.add(MaxPooling2D(2,2))
+    # model.add(Conv2D(128,(3,3),activation="relu"))
+    # model.add(MaxPooling2D(2,2))
 
-    # Layer III
-    model.add(Conv2D(128,(3,3),activation="relu"))
-    model.add(MaxPooling2D(2,2))
+    model.add(Flatten())        # Flatten the output
 
-    # Layer IV
-    model.add(Conv2D(256,(3,3),activation="relu"))
-    model.add(MaxPooling2D(2,2))
-
-    # Layer V
-    model.add(Conv2D(64,(3,3),activation="relu"))
-    model.add(MaxPooling2D(2,2))
-
-    # Flatten the input
-    model.add(Flatten())
-
-    # Layer VI Dense
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.2))
 
     # Output layer
-    model.add(Dense(8, activation="sigmoid"))
-
+    model.add(Dense(CLASSES, activation="sigmoid"))
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.fit(X_train, y_train, batch_size=bs, epochs=ep, validation_data = (X_test, y_test),  callbacks=[tensorboard])
 
     # Save model
     model.save('cnn.model')
+
+
+    model.summary()
 
 cnn_model(X_train, X_test, y_train, y_test)
 
